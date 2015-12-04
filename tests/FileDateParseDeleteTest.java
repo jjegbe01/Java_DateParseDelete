70c2cabb4f9e4d8f9c7bc70f1080895adf2cdf18
Julian Egbert <jjegbe01@louisville.edu>
2015-12-04 11:48:17 -0500

    Added test class for FIleDateParseDelete.java

---
+++ tests/FileDateParseDeleteTest.java
@@ -0,0 +1,19 @@
+package [PACKAGE];
+
+import java.text.ParseException;
+
+import org.junit.Test;
+
+import path.to.tested.class.FileDeleter;
+
+public class FileDeleterTest {
+
+	@Test
+	
+	public void testFileDeleter() throws ParseException
+	{
+//		final String exampleFilePath = "C:\\path\\to\\directory\\Test";
+//		FileDeleter fdOne = new FileDeleter();
+//		fdOne.DeleteFiles(exampleFilePath);
+	}
+}
