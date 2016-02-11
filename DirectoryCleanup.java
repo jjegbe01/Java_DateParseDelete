dc45ca6ac0e093e028108c71334d1203a79c16a3
Julian Egbert <jjegbe01@louisville.edu>
2016-02-11 09:50:06 -0500

    Added updated code
    Includes methods for working with an FTP server through an autowired properties file, commented out towards the bottom of the class.

--- DirectoryCleanup.java
+++ DirectoryCleanup.java
@@ -1,115 +1,101 @@
-package [PACKAGE.PATH.HERE];
+package [Package path goes here];
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.Properties;
+import org.apache.commons.net.ftp.FTPClient;
 
 public class DirectoryCleanup{
 
 	private static Calendar cal = Calendar.getInstance();
-	private static DateFormat df = new SimpleDateFormat("MMddyyyy");
+	private static DateFormat dfSecondary = new SimpleDateFormat("yyyyMMdd");
 	private Date fileDate;
 	private String tempDate;
 	private static Date limitDate;
 	private File[] listOfFiles;
 	private int startIndex;
 	private int lastIndex;
-	private String invalidRenameToTarget;
-	private String validRenameToTarget;
 	Exception exPrimary = null;
 	Exception exSecondary = null;
 	
-	/**Use only if you plan on having the application move invalid or outdated files to a new directory**/
-	//TODO: Rewrite as a relative file path before implementation, or rewrite as the target directory on the server
-	//String fileInput = "[D:\\PATH\\TO\\PROPERTIES\\FILE\\GOES\\HERE\\directorycleanup.properties]";
-	//Properties prop = new Properties();
-	//InputStream input = null;
-
-	/*
-	 * All methods in this class assume that each filename in a directory has the following naming convention:
-	 * filename_MMddyyyy.extension
-	 * [Example] : testFile1245_01012015.txt, abc_07042014.docx, etc.
-	 */
-
-	private void establishDate()
-	{
-		//Cutoff date can be adjusted here. Currently set to 31 days.
-		cal.add(Calendar.DATE, -31);
+	public DirectoryCleanup(){ }
+	
+	private void establishDate(int retentionLength){
+		cal.add(Calendar.DATE, -retentionLength);
 		limitDate = cal.getTime();
 	}
-	
-	//TODO: Externalize the process of retrieving the date from each filename
-	//Delete's all files in a target directory that are older than X days (See establishDate() method)
-	public void DeleteFiles(String filepath) throws ParseException, FileNotFoundException {
-		establishDate();
+
+	public void deleteFiles(String filepath, int retentionLength) throws ParseException, FileNotFoundException {
+		establishDate(retentionLength);
+		System.out.println("The cutoff date as of today is: " + limitDate.toString());
 		File folder = new File(filepath);
-		if (folder.listFiles().length > 0) 
+		int countedOutdatedFiles = 0;
+		int countedNewFiles = 0;
+		int countedInvalidFiles = 0;
+		if (folder.exists() == true)
 		{
-			listOfFiles = folder.listFiles();
-			for (File file : listOfFiles) {
-				startIndex = file.getName().lastIndexOf("_") + 1;
-				lastIndex = file.getName().lastIndexOf(".");
-				if (startIndex > 0 && lastIndex > 0) {
-					
-					/**Use only if you plan on having the application move invalid files to a new directory**/
-					/*try 
-					{
-						input = new FileInputStream(fileInput);
-						prop.load(input);
-						validRenameToTarget = prop.getProperty("deleter.valid.renameto.target");
-						invalidRenameToTarget = prop.getProperty("deleter.invalid.renameto.target");
-					} 
-					catch(IOException ex)
-					{
-						ex.printStackTrace();
-					}*/
-					
-					try {
-						tempDate = file.getName().substring(startIndex, lastIndex);
-						fileDate = df.parse(tempDate);
-						df.format(limitDate);
-					} catch (Exception ex) {
-						exPrimary = ex;
-						System.err.println("Caught exception: " + exPrimary.getMessage());
-						System.err.println("File " + file.getName() + " does not have a valid filename.");
-						System.err.println("File " + file.getName() + " will be ignored.");
-						
-						//System.err.println("File " + file.getName() + " has been moved to a different directory.");
-						//file.renameTo(new File(invalidRenameToTarget + file.getName()));
-						
-						continue;
-					}
-					if (fileDate.compareTo(limitDate) == 0
-							|| fileDate.compareTo(limitDate) < 0) {
-						 System.out.println(file.getName() + " is older than 30 days and will be deleted.");
-						 file.delete();
-						 System.out.println(file.getName() + " was deleted.");
-					}
-					else if (fileDate.compareTo(limitDate) > 0) {
-						System.out.println(file.getName() + " is not out of date.");
-					}
-				}		
+			if (folder.listFiles().length > 0) 
+			{
+				listOfFiles = folder.listFiles();
+				for (File file : listOfFiles) {
+					startIndex = file.getName().lastIndexOf("_") + 1;
+					lastIndex = file.getName().lastIndexOf(".");
+					if (startIndex > 0 && lastIndex > 0) {
+						try {
+							tempDate = file.getName().substring(startIndex, lastIndex);
+							fileDate = dfSecondary.parse(tempDate);
+							dfSecondary.format(limitDate);
+						} catch (Exception ex) {
+							exPrimary = ex;
+							System.err.println("Caught exception: " + exPrimary.getMessage());
+							System.err.println("File " + file.getName() + " does not have a valid filename.");
+							System.err.println("File " + file.getName() + " will be ignored.");
+							countedInvalidFiles += 1;
+							continue;
+						}
+						if (fileDate.compareTo(limitDate) <= 0) {
+							 System.out.println(file.getName() + " is older than 30 days and will be deleted.");
+							 file.delete();
+							 System.out.println(file.getName() + " was deleted.");
+							 countedOutdatedFiles += 1;
+						}
+						else if (fileDate.compareTo(limitDate) > 0) {
+							System.out.println(file.getName() + " is not out of date.");
+							countedNewFiles += 1;
+						}
+					}		
+				}
+				System.out.println("- - - - - - - - - - - - - - - - - - - -\nWithin the " + filepath.toString() + " directory, there were this many of each file type:");
+				System.out.println("Outdated files: " + countedOutdatedFiles);
+				System.out.println("Unparsable files: " + countedInvalidFiles);
+				System.out.println("Files less than 30 days old: " + countedNewFiles);
+			}
+			else
+			{
+				System.out.println("Specified directory is empty.");
 			}
 		}
 		else
 		{
-			System.out.println("Specified directory is empty.");
+			System.out.println("Specified directory does not exist: " + filepath.toString());
 		}
+	
 	}
 	
-	//Identifies all files in a target directory that are older than 30 days
-	public void identifyFiles(String filepath) throws ParseException {
-		establishDate();
-		File folder = new File(filepath);
+	public void identifyFiles(String filepath, String retentionLength) throws ParseException {
+	establishDate(Integer.parseInt(retentionLength));
+	System.out.println("The cutoff date as of today is: " + limitDate.toString());
+	File folder = new File(filepath);
+	System.out.println();
+	int countedOutdatedFiles = 0;
+	int countedNewFiles = 0;
+	int countedInvalidFiles = 0;
+	if(folder.exists() == true)
+	{
 		if (folder.listFiles().length > 0) 
 		{
 			listOfFiles = folder.listFiles();
@@ -119,79 +105,203 @@ public class DirectoryCleanup{
 				if (startIndex > 0 && lastIndex > 0) {
 					try {
 						tempDate = file.getName().substring(startIndex, lastIndex);
-						fileDate = df.parse(tempDate);
-						df.format(limitDate);
+						fileDate = dfSecondary.parse(tempDate);
+						dfSecondary.format(limitDate);
 					} catch (Exception ex) {
 						exPrimary = ex;
 						System.err.println("Caught exception: " + exPrimary.getMessage());
 						System.err.println("File " + file.getName() + " does not have a valid filename.");
+						countedInvalidFiles += 1;
 						continue;
 					}
-					if (fileDate.compareTo(limitDate) == 0
-							|| fileDate.compareTo(limitDate) < 0) {
+					if (fileDate.compareTo(limitDate) == 0 || fileDate.compareTo(limitDate) < 0) {
 						System.out.println(file.getName() + " is older than 30 days.");
+						countedOutdatedFiles += 1;
 					}
 					else if (fileDate.compareTo(limitDate) > 0) {
 						System.out.println(file.getName() + " is not out of date.");
+						countedNewFiles += 1;
 					}
 				}		
 			}
+			//Report results of scan
+			System.out.println("- - - - - - - - - - - - - - - - - - - -\nWithin the " + filepath.toString() + " directory, there were this many of each file type:");
+			System.out.println("Outdated files: " + countedOutdatedFiles);
+			System.out.println("Unparsable files: " + countedInvalidFiles);
+			System.out.println("Files less than 30 days old: " + countedNewFiles);
 		}
 		else
 		{
 			System.out.println("Specified directory is empty.");
 		}
 	}
+	else
+	{
+	System.out.println("Specified directory does not exist: " + filepath.toString());	
+	}
+}
 	
-	//Moves all files that are older than 30 days from the target directory to another directory
-	/*public void moveFiles(String filepath) throws ParseException {
-		establishDate();
-		File folder = new File(filepath);
-		if (folder.listFiles().length > 0) 
+	
+	/**Outdated methods, delete later.**/
+	/*public void getFTPProperties()
+	{
+		String ftpPropertiesInput = "Path to properties file goes here";
+		Properties prop = new Properties();
+		ftpConfigProperties = new FTPConfigProperties();
+		InputStream input = null;
+		try
 		{
-			listOfFiles = folder.listFiles();
-			for (File file : listOfFiles) {
+			input = new FileInputStream(ftpPropertiesInput);
+			prop.load(input);
+			ftpConfigProperties.setFtpHostName(prop.getProperty("dps.ftp.hostname"));
+			ftpConfigProperties.setFtpPort(prop.getProperty("dps.ftp.port"));
+			ftpConfigProperties.setFtpUsername(prop.getProperty("dps.ftp.username"));
+			ftpConfigProperties.setFtpPassword(prop.getProperty("dps.ftp.password"));
+			ftpConfigProperties.setFtpExtension(prop.getProperty("dps.ftp.file.extension"));
+			ftpConfigProperties.setFtpSqlDirPath(prop.getProperty("dps.sql.dirPath"));
+		}
+		catch(IOException ex)
+		{
+			ex.printStackTrace();
+		}
+	}*/
+	
+	/*public FTPClient getFTPClient(FtpUtils ftpUtils)
+	{
+		getFTPProperties();
+		try
+		{
+			String hostname = ftpConfigProperties.getFtpHostName();
+			int port = Integer.parseInt(ftpConfigProperties.getFtpPort());
+			String username = ftpConfigProperties.getFtpUsername();
+			String password = ftpConfigProperties.getFtpPassword();
+			if (port != 21)
+			{
+				port = 21;
+			}
+			ftpClient = ftpUtils.connect(hostname, port, username, password);
+			ftpClient.setRemoteVerificationEnabled(false);
+			ftpClient.enterLocalPassiveMode();
+		}
+		catch(IOException ex)
+		{
+			ex.printStackTrace();
+		}
+		return ftpClient;
+	}*/
+	/*	public void DeleteFTPFiles (FTPFile[] ftpFiles, FTPClient ftpClient) throws ParseException, IOException
+	{
+		establishDate();
+		System.out.println("The cutoff date as of today is: " + limitDate.toString());
+		if(ftpFiles.length > 0)
+		{
+			for (FTPFile file : ftpFiles)
+			{
 				startIndex = file.getName().lastIndexOf("_") + 1;
 				lastIndex = file.getName().lastIndexOf(".");
-				if (startIndex > 0 && lastIndex > 0) {
-					try 
+				if (startIndex > 0 && lastIndex > 0)
+				{
+					try
 					{
-						input = new FileInputStream(fileInput);
-						prop.load(input);
-						validRenameToTarget = prop.getProperty("deleter.valid.renameto.target");
-						invalidRenameToTarget = prop.getProperty("deleter.invalid.renameto.target");
-					} 
-					catch(IOException ex)
-					{
-						ex.printStackTrace();
-					}
-					try {
 						tempDate = file.getName().substring(startIndex, lastIndex);
-						fileDate = df.parse(tempDate);
-						df.format(limitDate);
-					} catch (Exception ex) {
+						fileDate = dfSecondary.parse(tempDate);
+						dfSecondary.format(limitDate);
+					}
+					catch (Exception ex)
+					{
 						exPrimary = ex;
 						System.err.println("Caught exception: " + exPrimary.getMessage());
-						System.err.println("File " + file.getName() + " does not have a valid filename.");		
-						System.err.println("File " + file.getName() + " has been moved to a different directory.");
-						file.renameTo(new File(invalidRenameToTarget + file.getName()));
+						System.err.println("File " + file.getName() + " does not have a valid filename.");
+						countedInvalidFiles += 1;
 						continue;
 					}
-					if (fileDate.compareTo(limitDate) == 0
-							|| fileDate.compareTo(limitDate) < 0) {
-						 System.out.println(file.getName() + " is older than 30 days and will be moved.");
-						 file.renameTo(new File(validRenameToTarget + file.getName()));
-						 System.out.println(file.getName() + " was moved to ." + validRenameToTarget.toString());
+					if(fileDate.compareTo(limitDate) == 0 || fileDate.compareTo(limitDate) < 0)
+					{
+						System.out.println(file.getName() + " is older than 30 days.");
+						System.out.println(file.getName() + " will be deleted.");
+						fileToDelete = ftpClient.printWorkingDirectory() + "/" + file.getName();
+						ftpClient.deleteFile(fileToDelete);
+						System.out.println(file.getName() + " was deleted.");
+						countedOutdatedFiles += 1;
 					}
-					else if (fileDate.compareTo(limitDate) > 0) {
-						System.out.println(file.getName()+ " is not out of date.");
+					else
+					{
+						System.out.println(file.getName() + " is not out of date.");
+						countedNewFiles += 1;
 					}
-				}		
+				}
+				
 			}
+			//Report results of scan
+			System.out.println("\n- - - - - - - - - - - - - - - - - - - -\nWithin the " + ftpClient.printWorkingDirectory() + " directory: ");
+			System.out.println(countedOutdatedFiles + " Outdated files were identified and deleted.");
+			System.out.println(countedInvalidFiles + " files with unparsable filenames were found.");
+			System.out.println(countedNewFiles + " files were found that are younger than 30 days.");
+			countedOutdatedFiles = 0;
+			countedInvalidFiles = 0;
+			countedNewFiles = 0;
 		}
 		else
 		{
-			System.out.println("Specified directory is empty.");
+			System.out.println("The target directory is empty.");
 		}
-	}*/	
+		ftpClient.logout();
+		System.out.println("Logged out of FTP server.");
+	}*/
+	
+	/*public void identifyFTPFiles (FTPFile[] ftpFiles, FTPClient ftpClient) throws ParseException, IOException
+	{
+		establishDate();
+		System.out.println("The cutoff date as of today is: " + limitDate.toString());
+		System.out.println("- - - - - - - - - - - - - - - - - - - -\n");
+		if(ftpFiles.length > 0)
+		{
+			for (FTPFile file : ftpFiles)
+			{
+				startIndex = file.getName().lastIndexOf("_") + 1;
+				lastIndex = file.getName().lastIndexOf(".");
+				if (startIndex > 0 && lastIndex > 0)
+				{
+					try
+					{
+						tempDate = file.getName().substring(startIndex, lastIndex);
+						fileDate = dfSecondary.parse(tempDate);
+						dfSecondary.format(limitDate);
+					} catch (Exception ex) {
+						exPrimary = ex;
+						System.err.println("Caught exception: " + exPrimary.getMessage());
+						System.err.println("File " + file.getName() + " does not have a valid filename.");
+						countedInvalidFiles += 1;
+						continue;
+					}
+					if (fileDate.compareTo(limitDate) == 0 || fileDate.compareTo(limitDate) < 0)
+					{
+						System.out.println(file.getName() + " is older than 30 days.");
+						countedOutdatedFiles += 1;
+					}
+					else if (fileDate.compareTo(limitDate) > 0)
+					{
+						System.out.println(file.getName() + " is not out of date.");
+						countedNewFiles += 1;
+					}
+				}
+			}
+			//Resets calendar pointer to current date after scanning each directory
+			cal.add(Calendar.DATE, 31);
+			//Report results of scan
+			System.out.println("\n- - - - - - - - - - - - - - - - - - - -\nWithin the " + ftpClient.printWorkingDirectory() + " directory, there were this many of each file type:");
+			System.out.println("Outdated files: " + countedOutdatedFiles);
+			System.out.println("Unparsable files: " + countedInvalidFiles);
+			System.out.println("Files less than 30 days old: " + countedNewFiles);
+			countedOutdatedFiles = 0;
+			countedInvalidFiles = 0;
+			countedNewFiles = 0;
+		}
+		else
+		{
+			System.out.println("The target directory is empty");
+		}
+		ftpClient.logout();
+		System.out.println("Logged out of FTP server.");
+	}*/
 }
